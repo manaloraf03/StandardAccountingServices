e9859202328e53967d985c7a6901534798017a8d
John Benedict Pangilinan <jbpangilinan245@gmail.com>
2017-12-06 11:21:50 +0800

    Add Excel and Email attachement to Purchase Invoice Report, Edit Email.php

--- system/libraries/Email.php
+++ system/libraries/Email.php
@@ -1974,46 +1974,100 @@ class CI_Email {
 	 *
 	 * @return	string
 	 */
-	protected function _smtp_connect()
-	{
-		if (is_resource($this->_smtp_connect))
-		{
-			return TRUE;
-		}
+	// protected function _smtp_connect()
+	// {
+	// 	if (is_resource($this->_smtp_connect))
+	// 	{
+	// 		return TRUE;
+	// 	}
 
-		$ssl = ($this->smtp_crypto === 'ssl') ? 'ssl://' : '';
+	// 	$ssl = ($this->smtp_crypto === 'ssl') ? 'ssl://' : '';
 
-		$this->_smtp_connect = fsockopen($ssl.$this->smtp_host,
-							$this->smtp_port,
-							$errno,
-							$errstr,
-							$this->smtp_timeout);
+	// 	$this->_smtp_connect = fsockopen($ssl.$this->smtp_host,
+	// 						$this->smtp_port,
+	// 						$errno,
+	// 						$errstr,
+	// 						$this->smtp_timeout);
 
-		if ( ! is_resource($this->_smtp_connect))
-		{
-			$this->_set_error_message('lang:email_smtp_error', $errno.' '.$errstr);
-			return FALSE;
-		}
+	// 	if ( ! is_resource($this->_smtp_connect))
+	// 	{
+	// 		$this->_set_error_message('lang:email_smtp_error', $errno.' '.$errstr);
+	// 		return FALSE;
+	// 	}
 
-		stream_set_timeout($this->_smtp_connect, $this->smtp_timeout);
-		$this->_set_error_message($this->_get_smtp_data());
+	// 	stream_set_timeout($this->_smtp_connect, $this->smtp_timeout);
+	// 	$this->_set_error_message($this->_get_smtp_data());
 
-		if ($this->smtp_crypto === 'tls')
-		{
-			$this->_send_command('hello');
-			$this->_send_command('starttls');
+	// 	if ($this->smtp_crypto === 'tls')
+	// 	{
+	// 		$this->_send_command('hello');
+	// 		$this->_send_command('starttls');
 
-			$crypto = stream_socket_enable_crypto($this->_smtp_connect, TRUE, STREAM_CRYPTO_METHOD_TLS_CLIENT);
+	// 		$crypto = stream_socket_enable_crypto($this->_smtp_connect, TRUE, STREAM_CRYPTO_METHOD_TLS_CLIENT);
 
-			if ($crypto !== TRUE)
-			{
-				$this->_set_error_message('lang:email_smtp_error', $this->_get_smtp_data());
-				return FALSE;
-			}
-		}
+	// 		if ($crypto !== TRUE)
+	// 		{
+	// 			$this->_set_error_message('lang:email_smtp_error', $this->_get_smtp_data());
+	// 			return FALSE;
+	// 		}
+	// 	}
 
-		return $this->_send_command('hello');
-	}
+	// 	return $this->_send_command('hello');
+	// }
+
+/**
+     * SMTP Connect
+     *
+     * @return    string
+     */
+    protected function _smtp_connect()
+    {
+        if (is_resource($this->_smtp_connect))
+        {
+            return TRUE;
+        }
+
+        $ssl = ($this->smtp_crypto === 'ssl') ? 'ssl://' : '';
+
+        $streamContext = stream_context_create([
+            'ssl' => [
+                'verify_peer'      => false,
+                'verify_peer_name' => false
+            ]
+        ]);
+
+        $this->_smtp_connect = stream_socket_client($ssl.$this->smtp_host.':'.$this->smtp_port,
+            $errno,
+            $errstr,
+            $this->smtp_timeout,
+            STREAM_CLIENT_CONNECT,
+            $streamContext);
+
+        if ( ! is_resource($this->_smtp_connect))
+        {
+            $this->_set_error_message('lang:email_smtp_error', $errno.' '.$errstr);
+            return FALSE;
+        }
+
+        stream_set_timeout($this->_smtp_connect, $this->smtp_timeout);
+        $this->_set_error_message($this->_get_smtp_data());
+
+        if ($this->smtp_crypto === 'tls')
+        {
+            $this->_send_command('hello');
+            $this->_send_command('starttls');
+
+            $crypto = stream_socket_enable_crypto($this->_smtp_connect, TRUE, STREAM_CRYPTO_METHOD_TLS_CLIENT);
+
+            if ($crypto !== TRUE)
+            {
+                $this->_set_error_message('lang:email_smtp_error', $this->_get_smtp_data());
+                return FALSE;
+            }
+        }
+
+        return $this->_send_command('hello');
+    } 
 
 	// --------------------------------------------------------------------
 
